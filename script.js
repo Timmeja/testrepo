aebc2b4c9099c3706a946f63414a8bb506cdf4f3
Timmeja <balla.timea03@gmail.com>
2021-09-09 21:26:10 +0200

    gyakorlás

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+//a script file-ban egy matematikai képlet eredményét tedd be egy template scriptbe és írd ki az eredményt konzolba
+let y=12;
+let z=3;
+let x=(y/z);
+console.log(`${y/z}`)
+console.log("${y/z}")
+console.log(x)
 
+
+//keresd ki és írj egy-egy egy soros tesztkódot a következő metódusokhoz:
+
+//charAt
+console.log ('Samu'.charAt(2))
+
+//concat
+const str1 = "Mihály";
+const str2 = "Balázs";
+const str3 = "Sámuel";
+
+console.log (str1.concat (' ', str2, ' ', str3))
+
+//startsWith
+
+const name = "Mihály Balázs Sámuel";
+console.log (name.startsWith ('Sámuel'))
+
+//substring
+
+console.log (str3.substring(0, 4));
+
+//trim
+
+const kidName= " Mihály Balázs Sámuel ";
+console.log (kidName.trim());
+
+if (!0){
+        var str4= "Balázs";
+    console.log(str4);    
+}
+    console.log(str4);
